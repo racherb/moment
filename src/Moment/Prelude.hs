9bfb251f0019df7bb15e2a5417b83bce7f9e410a
racherb <racherb@protonmail.com>
2020-07-10 01:54:29 -0400

    Remove redundant func from prelude
    Remove redundant func from prelude

--- src/Moment/Prelude.hs
+++ src/Moment/Prelude.hs
@@ -1,41 +1,44 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE CPP               #-}
+{-# LANGUAGE Unsafe            #-}
 
 module Moment.Prelude (
   module Exports
 ) where
 
 import Prelude as Exports (
-    read,
-    --show,
-    length,
-    take,
-    drop,
-    return,
-    (.),
-    ($),
-    null,
-    takeWhile,
-    scanl,
-    replicate,
-    --ceiling,
-    concat,
-    lookup,
-    elem,
-    replicate,
-    foldr,
-    Functor(..)
+      read
+    , length
+    , take
+    , drop
+    , return
+    , (.)
+    , ($)
+    , null
+    , takeWhile
+    , scanl
+    , replicate
+    , concat
+    , lookup
+    , elem
+    , replicate
+    , foldr
+    , Functor(..)
     )
 
-import Data.Tuple as Exports
-import Data.Maybe as Exports
-import Data.String as Exports
+import Data.Tuple   as Exports
+import Data.Maybe   as Exports
+import Data.String  as Exports
 
-import GHC.IO as Exports
-import GHC.Num as Exports
-import GHC.Real as Exports
-import GHC.Show as Exports
-import GHC.Read as Exports
+import GHC.IO       as Exports -- ^ Unsafe. The module itself isn't safe.
+import GHC.Prim     as Exports -- ^ Unsafe. The module itself isn't safe.
+import GHC.Types    as Exports -- ^ Unsafe. The module itself isn't safe.
+import GHC.Exts     as Exports
+import GHC.Num      as Exports
+import GHC.Real     as Exports
+import GHC.Show     as Exports
+import GHC.Read     as Exports
+import GHC.Magic    as Exports
 
 -- Base types
 import Data.Int     as Exports
@@ -43,11 +46,11 @@ import Data.Bool    as Exports hiding (bool)
 import Data.Char    as Exports (Char)
 
 -- Base typeclasses
-import Data.Eq as Exports
-import Data.Ord as Exports
+import Data.Eq        as Exports
+import Data.Ord       as Exports
 #if MIN_VERSION_base(4,9,0)
-import Data.Monoid as Exports (Monoid(..), mempty)
-import Data.Semigroup as Exports (Semigroup(..), (<>))
+import Data.Monoid    as Exports (Monoid(..), mempty)
+import Data.Semigroup as Exports (Semigroup(..), (<>)) -- ^ Since: 4.9.0.0
 #else
-import Data.Monoid as Exports (Monoid(..), mempty, (<>))
+import Data.Monoid    as Exports (Monoid(..), mempty, (<>))
 #endif
