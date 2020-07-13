0e585f144a3949bbc426c194723054bc94e59149
racherb <racherb@protonmail.com>
2020-07-12 21:42:43 -0400

    Add new funcs benchmarks
    Add new funcs benchmarks

--- bench/DaysCalendarBench.hs
+++ bench/DaysCalendarBench.hs
@@ -1,15 +1,35 @@
 import Gauge
 import qualified Data.Vector as V
 import Moment.Calendar.Internals
+import Prelude hiding (or, and, reverse)
 
 main :: IO ()
 main =
-    defaultMain $ map group [5, 100, 1000]
+    defaultMain $ map group [100, 1000]
   where
     group size =
         bgroup (show size)
-          [ bench "normalize x" $ whnf normalize x
+          [ 
+            bench "normalize x" $ whnf normalize x
           , bench "normalize u<>v<>k" $ whnf normalize (u<>v<>k)
+          , bench "and" $ whnf andx y
+          , bench "or" $ whnf orx y
+          , bench "match" $ whnf matchx y
+          , bench "add" $ whnf addx y
+          , bench "sustract" $ whnf sustractx y
+          , bench "holes" $ whnf holesx y
+          , bench "reverse" $ whnf reverse x
+          , bench "ones" $ whnf ones x
+          , bench "zeros" $ whnf zeros x
+          , bench "oddd" $ whnf oddd y
+          , bench "evend" $ whnf evend z
+          , bench "make" $ whnf maket (V.fromList [1,0,0,0,1,1,1,0,0,0,0,1,1,1,1])
+          , bench "move 1" $ whnf move1 x
+          , bench "move 1000" $ whnf move1000 x
+          , bench "toDates" $ whnf toDates z
+          , bench "step" $ whnf step1230 y
+          , bench "pulse" $ whnf pulse13430 z
+          , bench "section" $ whnf section23306 y
           ]
       where
         x = step 1 1 10 $ make 2020 01 (V.fromList [])
@@ -18,4 +38,16 @@ main =
         u = x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z
         v = x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z
         k = x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z<>x<>y<>z
+        andx = and x
+        orx = or x
+        addx = add x
+        sustractx = sustract x
+        holesx = holes x
+        matchx = Moment.Calendar.Internals.match x
+        maket = make 1979 11
+        move1 = move 1
+        move1000 = move 1000
+        step1230 = step 1 2 30
+        pulse13430 = pulse 1 3 4 30
+        section23306 = section 2 (3, 30) 6
         
